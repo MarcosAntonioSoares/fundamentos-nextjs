1b8cc661a1fa59b013a4bcde2ee50333097288be
Marcos <marcosas.soares2@gmail.com>
2021-11-28 11:40:58 -0300

    alteração package.json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() { 
+  render() {
     return (
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-          
+
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
